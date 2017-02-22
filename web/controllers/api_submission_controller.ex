1fa1c4570a6713786b76397a373b92a7382fc88d
Reiko <reiko.weiss@posteo.de>
2017-02-22 19:10:09 +0100

    add API Submission Index with query arguments

--- web/controllers/api_submission_controller.ex
+++ web/controllers/api_submission_controller.ex
@@ -15,4 +15,27 @@ defmodule TouchRecorderWeb.ApiSubmissionController do
         |> send_resp(501, changeset.errors)
     end
   end
+
+  def index(conn, %{
+    "challenge" => ch_id,
+    "day" => ch_day,
+    "month" => ch_month,
+    "year" => ch_year
+    }) do
+
+    query = from s in Submission,
+    where: [challenge_id: ^ch_id],
+    where: s.inserted_at >= type(^Ecto.Date.from_erl(
+    {
+      String.to_integer(ch_year),
+      String.to_integer(ch_month),
+      String.to_integer(ch_day)
+    }),Ecto.Date),
+    order_by: [desc: s.inserted_at],
+    select: [s.archive]
+
+    submission = Repo.all(query)
+    json conn, %{ submission: submission }
+  end
+
 end
